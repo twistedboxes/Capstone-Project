4770640f307d9c4711d7a61c8e597498d0421d23
twistedboxes <tamponiandrea@gmail.com>
2024-06-16 18:13:18 +0200

    create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,11 @@
 import React from "react";
 import "../App.css"
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <header className="header">
-      <nav className="navbar">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_vsRr6NGdZxBWI2M8SVGdfpsEzvyir7tPog&s" alt="logo" />
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Menu</a>
-          </li>
-          <li>
-            <a href="#">Reservations</a>
-          </li>
-          <li>
-            <a href="#">Order Online</a>
-          </li>
-          <li>
-            <a href="#">Login</a>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
     </header>
   );
 };
