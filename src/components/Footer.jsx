8897f7c3ba2c7e3aab1f26b2ccbc31d8a1ea1ee0
twistedboxes <tamponiandrea@gmail.com>
2024-06-16 12:23:30 +0200

    created basic semantic structure and components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="" alt="logo" />
+      <div>
+        <h3>Doormat Navigation</h3>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Reservations</a>
+          </li>
+          <li>
+            <a href="#">Order Online</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>email</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>email</li>
+        </ul>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="" alt="logo" />
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Reservations</a>
+          </li>
+          <li>
+            <a href="#">Order Online</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
