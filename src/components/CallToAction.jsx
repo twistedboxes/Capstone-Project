94decf6784d8d96afbf9a31389c300b3d26272c3
twistedboxes <tamponiandrea@gmail.com>
2024-06-16 18:13:43 +0200

    create cta section

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <section className='hero-section'>
+        <h1>IM THE HEROSECTION</h1>
+    </section>
+  )
+}
+
+export default CallToAction
